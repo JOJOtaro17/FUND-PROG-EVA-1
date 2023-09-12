8a36a9f31956fba96526872ed67b0ce9e6d486e6
JOJOtaro17 <143568674+JOJOtaro17@users.noreply.github.com>
2023-09-11 18:49:15 -0700

    Add files via upload

---
+++ EVA1_6_ENTRADAS_SALIDAS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_6_entradas_salidas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_6_ENTRADAS_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int año;
+        //Hay un paquete Syste, dentro un paquete out, y dentro de out, la instruccion println
+        System.out.println("Introduce la marca del auto");
+        System.out.println("Introduce el modelo del auto");
+        System.out.println("Introduce el aÃ±o del auto");
+    }
+   
+}
