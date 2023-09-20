7ad22be995df4244e0c4b763977c01a154495dc7
JOJOtaro17 <143568674+JOJOtaro17@users.noreply.github.com>
2023-09-19 18:51:12 -0700

    Add files via upload

---
+++ EVA1_11_INICIALIZACIÓN/src/eva1_11_inicialización/EVA1_11_INICIALIZACIÓN.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_11_inicialización;
+
+/**
+ *
+ * @author Joshua
+ */
+public class EVA1_11_INICIALIZACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //println()----funcion
+        //main()----- funcion
+        System.out.println("hola mundo");
+        int valor = 0;//inicializar la variable para usarla
+        System.out.println(valor);
+        //declaramos e inicializamos 3 variables double
+        double salario = 0,iva = 0.16,subtotal = 10000;
+        
+        final int CALIFA_PASAR = 70;
+        final String ESCUELA= "instituto ntecnologico de chihuahua II";
+        
+        System.out.println(ESCUELA);
+        System.out.println("La califa minima para pasar es:");
+        System.out.println(CALIFA_PASAR);
+        
+        //error: solo se puede asignar una vez
+        //califa_pasar=100;
+        //califa_pasar=20;
+        byte valores=120;
+        //valores=128;
+        int miValor = Integer.MAX_VALUE;
+        System.out.println(miValor+1);
+        
+    }
+    
+}
