39a5953f18da7ffbd68ae4fdeeffea500cba67f5
JOJOtaro17 <143568674+JOJOtaro17@users.noreply.github.com>
2023-10-01 14:06:39 -0700

    Add files via upload

---
+++ UNIBUS(1)FUNDAMENTO-PROGA((EXA-1 DE EVALUACION)/src/unibus/UNIBUS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unibus;
+
+/**
+ *
+ * @author Joshua
+ */
+public class UNIBUS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nequip= "icecream";
+        String proyecfin= "unibus";
+        String integrante= "Joshua Jahir Gurrola Zarate-23550404";
+        String integrante2="Regina Olivas Beltran-23550367";
+        String integrante3="Judith Acosta Alvares-23550398";
+        String integrante4="Saul Alejandro Bermudez Arzaga-23550382";
+        System.out.println("nombre del equipo: "+ nequip);
+        System.out.println("nombre de los integrantes del equipo:");
+        System.out.println("- " +integrante);
+        System.out.println("- " +integrante2);
+        System.out.println("- " +integrante3);
+        System.out.println("- " +integrante4);
+        System.out.println("nombre del proyecto: "+ proyecfin);
+                
+                
+    }
+    
+}
